5ee722233a5c2e481022d3ae6fa490ecc9e9591b
trefania <tarirotrefania@gmail.com>
2023-07-14 19:29:54 +0200

    about us

--- frontend work/Main-page/Landing-page/profile-page/script.js
+++ frontend work/Main-page/Landing-page/profile-page/script.js
@@ -1,9 +1,4 @@
-function toggleSidebar() {
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.style.left = sidebar.style.left === '0px' ? '-250px' : '0px';
-}
-
-async function sendMessage() {
+function sendMessage() {
   const userInput = document.getElementById('userInput');
   const message = userInput.value;
   if (message.trim() !== '') {
@@ -16,24 +11,22 @@ async function sendMessage() {
     chatBody.scrollTop = chatBody.scrollHeight;
 
     // Send user message to the server and receive JSON response
-    const response = await fetch('/path-to-your-server-endpoint', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
+    $.ajax({
+      url: '/profile/chat/chat_id',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ message }),
+      success: function(jsonResponse) {
+        const botMessage = document.createElement('div');
+        botMessage.classList.add('message', 'received');
+        botMessage.innerHTML = `<p>${jsonResponse.message}</p>`;
+        chatBody.appendChild(botMessage);
+        chatBody.scrollTop = chatBody.scrollHeight;
       },
-      body: JSON.stringify({ message })
+      error: function(xhr, status, error) {
+        // Handle server error
+        console.error('Error:', error);
+      }
     });
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      const botMessage = document.createElement('div');
-      botMessage.classList.add('message', 'received');
-      botMessage.innerHTML = `<p>${jsonResponse.message}</p>`;
-      chatBody.appendChild(botMessage);
-      chatBody.scrollTop = chatBody.scrollHeight;
-    } else {
-      // Handle server error
-      console.error('Error:', response.status);
-    }
   }
 }
